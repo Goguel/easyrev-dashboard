7e1dcd2867553d33a7cc583071d336a3bf283694
Miguel Xavier <94547410+Goguel@users.noreply.github.com>
2025-07-07 22:54:38 -0300

    Initial commit

---
+++ backend/database/migrations/2025_07_08_014108_create_reservations_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+         Schema::create('reservations', function (Blueprint $table) {
+        $table->id(); // ID [cite: 88]
+        $table->string('guest_name'); // Nome do hóspede 
+        $table->date('check_in_date'); // Data de entrada 
+        $table->date('check_out_date'); // Data de saída 
+        $table->integer('guest_count'); // Quantidade de hóspedes 
+        $table->string('status')->default('pendente'); // Status, com 'pendente' como padrão 
+        $table->timestamps();
+    });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('reservations');
+    }
+};
